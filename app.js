f1c71aec72f97a03e274449a714ce1afd7d698fd
Tiffani Hess <hesstjune@gmail.com>
2020-10-04 12:45:01 -0500

    added different icon for when hamburger open

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const menuContainer = document.getElementsByClassName("nav-link-container")[0];
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
+  const hamburgerIcon = document.getElementById("hamburger-icon");
+
   menu.classList.toggle("open-menu");
   menuContainer.classList.toggle("open");
+  if (hamburgerIcon.classList.contains("fa-bars")) {
+    hamburgerIcon.classList.replace("fa-bars", "fa-times");
+  } else {
+    hamburgerIcon.classList.replace("fa-times", "fa-bars");
+  }
 });
