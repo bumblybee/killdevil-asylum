130dce756b11795c8c7d87a560e3132bec8ac584
Tiffani Hess <hesstjune@gmail.com>
2020-10-04 15:06:22 -0500

    enlarged hamburger icon and padding, set z index higher than corn header so can click on links

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const menu = document.getElementsByClassName("menu")[0];
-
-const menuContainer = document.getElementsByClassName("nav-link-container")[0];
-
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
   const hamburgerIcon = document.getElementById("hamburger-icon");
+  const menu = document.getElementsByClassName("menu")[0];
+
+  const menuContainer = document.getElementsByClassName(
+    "nav-link-container"
+  )[0];
 
   menu.classList.toggle("open-menu");
   menuContainer.classList.toggle("open");
+
   if (hamburgerIcon.classList.contains("fa-bars")) {
     hamburgerIcon.classList.replace("fa-bars", "fa-times");
   } else {
     hamburgerIcon.classList.replace("fa-times", "fa-bars");
-    console.log(window.location.href);
+
     hamburger.href = window.location.href;
   }
 });
